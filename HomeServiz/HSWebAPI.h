88332a6536c5df672a846339268607167d179dc0
Bheema Badri <Bheema.Badri@Admins-MacBook-Pro-4.local>
2018-04-26 17:49:39 +0530

    removed waste files

---
+++ HomeServiz/HSWebAPI.h
@@ -0,0 +1,13 @@
+//
+//  HSWebAPI.h
+//  HomeServiz
+//
+//  Created by Bheema Badri on 26/04/18.
+//  Copyright © 2018 Bheema Badri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HSWebAPI : NSObject
+
+@end
